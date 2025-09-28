97052f625f76db63ff4127597e30633fdb466846
Samir Khanal <gdssamir@gmail.com>
2025-09-28 15:41:35 +0545

    feat: setup local strategy

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common'
-
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+  async validateLocalUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email)
+    if (!user) throw new UnauthorizedException('User not found!')
+    const isPasswordMatched = await this.userService.comparePassword(user.password, password)
+    if (!isPasswordMatched) throw new UnauthorizedException('Invalid Credentials!')
+    return { id: user.id, role: user.role }
+  }
+}
