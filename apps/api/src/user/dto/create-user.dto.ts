26a0249c91342cd255f0de0c8546def4415b02ee
Samir Khanal <gdssamir@gmail.com>
2025-10-01 10:31:01 +0545

    refact: userschema

--- apps/api/src/user/dto/create-user.dto.ts
+++ apps/api/src/user/dto/create-user.dto.ts
@@ -2,11 +2,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  firstName: string
-
-  @IsString()
-  @IsNotEmpty()
-  lastName: string
+  name: string
 
   @IsString()
   @IsEmail()
