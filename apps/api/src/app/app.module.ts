80711012a6dd29f2071461eb1d6f0c0e3e1abc94
Samir Khanal <gdssamir@gmail.com>
2025-10-01 07:51:07 +0545

    refact: makle prisma global module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from 'src/auth/auth.module'
+import { UserModule } from 'src/user/user.module'
+import { PrismaModule } from 'src/prisma/prisma.module'
 
 @Module({
   imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
