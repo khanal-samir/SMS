1d9fd9d6acc895f1f331af85c65f81f04f8a399e
Samir Khanal <gdssamir@gmail.com>
2025-12-06 14:21:07 +0545

    fix: build

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common'
-import { Public } from 'src/auth/decorators/public.decorator'
 import * as schemas from '@repo/schemas'
+import { Public } from '../auth/decorators/public.decorator'
 @Controller()
 export class AppController {
   @Public()
