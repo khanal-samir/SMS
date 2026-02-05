e52de85b912d44fc0668159af149490a8281c652
Samir Khanal <gdssamir@gmail.com>
2026-02-05 11:22:02 +0545

    feat: add Semester and Subject controllers with CRUD operations

--- apps/api/src/semester/semester.module.ts
+++ apps/api/src/semester/semester.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { SemesterService } from './semester.service'
+import { SemesterController } from './semester.controller'
 
 @Module({
+  controllers: [SemesterController],
   providers: [SemesterService],
   exports: [SemesterService],
 })
